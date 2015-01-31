b33ea6c89e0ccfa9c84bfd02f408dba0f4ff6ab2
Ana Phan <anaphan@wireless-10-148-166-7.public.utexas.edu>
2015-01-30 20:25:52 -0600

    Removed the data frames and formed separate plots code R script

---
+++ 01 Data/plots.R
@@ -0,0 +1,11 @@
+df_combined <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from orders o join customers c on o.customer_id = c.customer_id join order_details d on o.order_id = d.order_id join items i on d.item_id = i.item_id"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+head (dfcombined)
+
+p1 <- ggplot (dfcombined, aes(x= as.Date(ORDER_DATE), y = as.Date(SHIPPED_DATE))) + geom_point(aes(color = as.factor(UNIT_PRICE)))
+p1 + facet_wrap (~ CUSTOMER_STATE)
+
+p2 <- ggplot (dfcombined, aes(x= as.Date(ORDER_DATE), y = as.Date(SHIPPED_DATE))) + geom_point(aes(color = as.factor(TITLE)))
+p2 + facet_wrap (~ CUSTOMER_STATE)
+
+p3 <- ggplot (dfcombined, aes(x= as.Date(ORDER_DATE), y = as.Date(SHIPPED_DATE))) + geom_point(aes(color = ARTIST))
+p3 + facet_wrap(~ CUSTOMER_STATE)
