d1e17d8c32cb50a58ca6dc9fb8af01ab70e77210
Parth Patel <parthu04@gmail.com>
2015-02-04 23:05:26 -0600

    Finishing up and checking on everything

--- 02 Visualizations/plot 5.R
+++ 02 Visualizations/plot 5.R
@@ -1,2 +1,2 @@
-p5 <- ggplot (df_orders, aes(x= CUSTOMER_ID, y = as.Date(ORDER_DATE))) + geom_point()
+p5 <- ggplot (df_orders, aes(x= as.Date(ORDER_DATE, "%Y-%m-%d"), y = as.Date(SHIPPED_DATE, "%Y-%m-%d"))) + geom_point(aes(color = as.factor(CUSTOMER_ID)))
 p5
